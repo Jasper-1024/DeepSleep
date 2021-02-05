c7d89b3bbc63a2e7c72d287e3325681176881c0d
win <ljy087621@gmail.com>
2021-02-05 11:49:14 +0800

    ui 导航组件,还遗留有导航菜单的bug 暂时无解

--- app/src/main/java/com/js/deepsleep/ui/MainActivity.kt
+++ app/src/main/java/com/js/deepsleep/ui/MainActivity.kt
@@ -1,12 +1,57 @@
 package com.js.deepsleep.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.js.deepsleep.R
+import com.js.deepsleep.base.LogUtil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 初始化
+        drawerLayout = findViewById(R.id.drawer_layout)
+        toolbar = findViewById(R.id.toolbar)
+
+//        val navController = findNavController(R.id.nav_host_fragment)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.appFragment), drawerLayout)
+
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
+
+        //设置侧边栏颜色
+        drawerLayout.setStatusBarBackground(R.color.colorPrimaryDark)
+        //Toolbar
+        setSupportActionBar(toolbar)// toolbar 替换 ActionBar
+
+    }
+
+    //ToolBar menu
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    fun test2(menu: MenuItem) {
+        LogUtil.d("test", "test2")
     }
 }
