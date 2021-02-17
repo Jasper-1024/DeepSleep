cdb0b759d5f5b3bf28be0f4a729b3f6de76c7415
win <ljy087621@gmail.com>
2021-02-17 23:13:29 +0800

    接入 Alipay

---
+++ app/src/main/java/com/js/deepsleep/ui/about/AliPay.kt
@@ -0,0 +1,45 @@
+package com.js.deepsleep.ui.about
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import com.js.deepsleep.base.LogUtil
+import java.net.URLEncoder
+
+class AliPay(private var context: Context) {
+    private val ALIPAY_SHOP = "https://qr.alipay.com/tsx168537dnhbsjucjfvl8f"//商户
+    private val ALIPAY_PERSON =
+        "https://qr.alipay.com/fkx16881wxcpvxbnwqkur21?t=1613570875134"//个人(支付宝里面我的二维码)
+
+    fun jumpAlipay() {
+        openAliPay2Pay(ALIPAY_PERSON)
+    }
+
+    private fun openAliPay2Pay(qrCode: String) {
+        openAlipayPayPage(context, qrCode)
+    }
+
+    private fun openAlipayPayPage(context: Context, qrcode: String): Boolean {
+        var encodeedQrcode = qrcode
+        try {
+            encodeedQrcode = URLEncoder.encode(qrcode, "utf-8")
+        } catch (e: Exception) {
+            LogUtil.d("Alipay", "$e")
+        }
+        try {
+            val aliPayQr =
+                "alipayqr://platformapi/startapp?saId=10000007&clientVersion=3.7.0.0718&qrcode=$encodeedQrcode"
+            val url = aliPayQr + "%3F_s%3Dweb-other&_t=" + System.currentTimeMillis()
+            openUri(context, url)
+            return true
+        } catch (e: Exception) {
+            LogUtil.d("Alipay", "$e")
+        }
+        return false
+    }
+
+    private fun openUri(context: Context, s: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(s))
+        context.startActivity(intent)
+    }
+}
