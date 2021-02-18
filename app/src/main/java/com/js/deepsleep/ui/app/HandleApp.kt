3d2e878a01fca712feab61fc537c97533f384489
win <ljy087621@gmail.com>
2021-02-18 10:36:10 +0800

    setting - 添加 extend 开关

--- app/src/main/java/com/js/deepsleep/ui/app/HandleApp.kt
+++ app/src/main/java/com/js/deepsleep/ui/app/HandleApp.kt
@@ -2,6 +2,8 @@ package com.js.deepsleep.ui.app
 
 import android.view.View
 import androidx.navigation.findNavController
+import androidx.preference.PreferenceManager
+import com.js.deepsleep.BasicApp
 import com.js.deepsleep.data.db.entity.AppInfo
 import com.js.deepsleep.data.db.entity.AppSt
 import com.js.deepsleep.ui.databinding.item.BaseItemHandle
@@ -20,11 +22,20 @@ class HandleApp(private val appViewModel: AppViewModel) : BaseItemHandle() {
     }
 
     fun onClick(view: View, appInfo: AppInfo) {
-        val direction = AppFragmentDirections.actionAppFragmentToExtendFragment(
-            appInfo.packageName,
-            appInfo.label
-        )
-        view.findNavController().navigate(direction)
+
+        // 获取拓展功能是否开启
+        val sharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(BasicApp.context)
+        val extendEnable = sharedPreferences.getBoolean("ExtendEnable", false)
+
+        if (extendEnable) {
+            // 跳转 extend
+            val direction = AppFragmentDirections.actionAppFragmentToExtendFragment(
+                appInfo.packageName,
+                appInfo.label
+            )
+            view.findNavController().navigate(direction)
+        }
     }
 
 //    fun onLongClick(view: View, appInfo: AppInfo): Boolean {
