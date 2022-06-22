d80edd2dc0bf790fa9cb9e4a5852dba9eaeadd9c
JasperHale <ljy087621@gmail.com>
2022-06-22 18:11:13 +0800

    getAppInfo

--- app/src/main/java/com/js/deepsleep/data/repository/app/AppRepo.kt
+++ app/src/main/java/com/js/deepsleep/data/repository/app/AppRepo.kt
@@ -1,11 +1,13 @@
 package com.js.deepsleep.data.repository.app
 
 import com.js.deepsleep.data.db.entity.App
+import com.js.deepsleep.data.db.entity.AppInfo
 import com.js.deepsleep.data.db.entity.AppSt
 import kotlinx.coroutines.flow.Flow
 
 interface AppRepo {
     fun getApps(): Flow<List<App>>
+    suspend fun getAppInfo(packageName: String): AppInfo
     suspend fun setAppSt(appSt: AppSt)
     suspend fun syncAppList()
 }
